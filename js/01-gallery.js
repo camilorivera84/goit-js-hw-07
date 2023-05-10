b8507fd3f4bf4249c93d294120ad0fb5bdc39dc0
unknown <ing.camilorivera@hotmail.es>
2023-05-10 13:06:19 -0500

    librerias y codigo java avance

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,52 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+//se toman las imagenes y se importan
+
+const gallery = document.querySelector('.gallery');
+
+// se crea la funcion para crear la galeria y los elementos
+
+function createGallery(items) {
+  return items
+    .map(
+      (item) =>
+        `<li class = "gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+  <img class= "gallery__image"
+  src="${item.preview}";
+  data-source="${item.original}"
+  alt="${item.description}"
+  /></a>
+  </li>`
+    )
+    .join('');
+  //se usa join para que todas las imagenes esten dentro de una misma matriz
+}
+
+//se crea la constante para añadir la galeria dentro de html usando el innerHTML
+
+const addGallery = createGallery(galleryItems);
+gallery.innerHTML = addGallery;
+gallery.addEventListener('çlick', clickOnImage);
+
+//se define la funcion clickOnImage para que abra la dentro de un modal
+
+function clickOnImage(imageAction) {
+  action(imageAction);
+  if (imageAction.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightBox.create(
+    `<img src="${imageAction.target.dataset.source}" width = "800" height="600">`
+  );
+  instance.show();
+  // se agrega un even listener para pulsar la tecla escapese cierre la imagen
+  gallery.addEventListener('keydown', (imageAction) => {
+    if (imageAction.code === 'Escape') {
+      instance.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const containerGallery = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(
+    (element) => `<li class= "gallery__item">
+<a class = "gallery__link" href = "${element.original}">
+<img class= "gallery__image"
+src = "${element.preview}"
+alt = "${element.description}"
+/>
+</a>
+</li>`
+  )
+  .join('');
+
+containerGallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+containerGallery.addEventListener('click', (event) => event.preventDefault());
+
+// uso de la libreria simpleLightBox
+
+let lightbox = new SimpleLightBox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
