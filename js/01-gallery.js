d3386bd0e10efaa43f3e9ec01abee990f82e8a45
unknown <ing.camilorivera@hotmail.es>
2023-05-11 20:52:32 -0500

    ajustes tarea 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -13,12 +13,12 @@ const builderGallery = galleryItems
   .map(
     (item) =>
       `<li class = "gallery__item">
-  <a class="gallery__link" href="${item.original}">
+  <a class = "gallery__link" href="${item.original}">
   <img class= "gallery__image"
-  src="${item.preview}";
+  src="${item.preview}"
   data-source="${item.original}"
-  alt="${item.description}"
-  /></a>
+  alt="${item.description}"/>
+  </a>
   </li>`
   )
   .join('');
