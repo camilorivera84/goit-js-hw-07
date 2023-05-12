ea2069c40b0a2ddf43ce118602e22d74dc14fdee
unknown <ing.camilorivera@hotmail.es>
2023-05-11 19:38:49 -0500

    ajustes tarea 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -23,3 +23,29 @@ alt = "${element.description}"
 
 containerGallery.insertAdjacentHTML('afterbegin', galleryMarkup);
 containerGallery.addEventListener('click', (event) => event.preventDefault());
+
+//se crea la constante para añadir la galeria dentro de html usando el innerHTML
+
+const addGallery = createGallery(galleryItems);
+gallery.innerHTML = addGallery;
+gallery.addEventListener('click', clickOnImage);
+
+//se define la funcion clickOnImage para que abra la dentro de un modal
+
+function clickOnImage(imageAction) {
+  action(imageAction);
+  if (imageAction.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${imageAction.target.dataset.source}" width = "800" height="600">`
+  );
+  instance.show();
+  // se agrega un even listener para pulsar la tecla escapese cierre la imagen
+  gallery.addEventListener('keydown', (imageAction) => {
+    if (imageAction.code === 'Escape') {
+      instance.close();
+    }
+  });
+}
