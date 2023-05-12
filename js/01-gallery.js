73c6291d7188e55af602a4179eacc526d13b5cfe
unknown <ing.camilorivera@hotmail.es>
2023-05-11 19:26:41 -0500

    correccion codigo js tarea 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -13,6 +13,7 @@ const galleryMarkup = galleryItems
 <a class = "gallery__link" href = "${element.original}">
 <img class= "gallery__image"
 src = "${element.preview}"
+data-source = "${element.original}"
 alt = "${element.description}"
 />
 </a>
