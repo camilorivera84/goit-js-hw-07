a03e45a56aa335c88e6ad0ef11b0389fc338613b
unknown <ing.camilorivera@hotmail.es>
2023-05-11 20:24:08 -0500

    ajustes tarea 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -11,7 +11,7 @@ function createGallery(items) {
   return items
     .map(
       (item) => `<li class= "gallery__item">
-<a class = "gallery__link" href = "${item.original}">
+<a class = "gallery__link" href = "large-image.jpg">
 <img class= "gallery__image"
 src = "${item.preview}"
 data-source = "${item.original}"
